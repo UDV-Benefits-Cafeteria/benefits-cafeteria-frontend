f7b81c4e4603fba6623473814380e1593c0e0763
profitrolka <turkin.2004@inbox.ru>
2024-09-30 02:43:10 +0500

    fix: Text props

--- src/shared/ui/Text/index.tsx
+++ src/shared/ui/Text/index.tsx
@@ -12,7 +12,7 @@ type TText = {
 } & React.DetailedHTMLProps<React.HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>;
 
 export const Text: FC<TText> = props => {
-  const { children, size = "m", fontStyle, className, boldness = "normal" } = props;
+  const { children, size = "m", fontStyle = "main", className, boldness = "normal" } = props;
   const isAlternative = fontStyle !== "main";
 
   return (
