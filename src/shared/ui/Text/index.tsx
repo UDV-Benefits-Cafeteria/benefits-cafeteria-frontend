ac36475b1ee67e8696d6bfd98425e23b55066219
profitrolka <turkin.2004@inbox.ru>
2024-10-06 01:56:49 +0500

    fix: text styles

--- src/shared/ui/Text/index.tsx
+++ src/shared/ui/Text/index.tsx
@@ -5,26 +5,19 @@ import { classNames } from "@shared/lib/classNames/classNames";
 import styles from "./Text.module.scss";
 
 type TText = {
-  size?: "s" | "m" | "l";
   boldness?: "normal" | "medium" | "bold";
   fontStyle?: "main" | "alternative";
   children: ReactNode;
 } & React.DetailedHTMLProps<React.HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>;
 
 export const Text: FC<TText> = props => {
-  const { children, size = "m", fontStyle = "main", className, boldness = "normal" } = props;
+  const { children, fontStyle = "main", className, boldness = "normal" } = props;
   const isAlternative = fontStyle !== "main";
 
   return (
     <p
       {...props}
-      className={classNames(
-        styles.text,
-        styles[size],
-        isAlternative ? styles.alternative : null,
-        styles[boldness],
-        className
-      )}
+      className={classNames(styles.text, isAlternative ? styles.alternative : null, styles[boldness], className)}
     >
       {children}
     </p>
