89fa4c3a0d3df66915828d35a23d2c3fce191469
profitrolka <turkin.2004@inbox.ru>
2024-10-16 01:36:02 +0500

    chore: add routes

---
+++ src/entity/User/model/slice/IUser.slice.ts
@@ -0,0 +1,24 @@
+import { type PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+import { TUserSliceSchema } from "../types/User.types";
+
+const initialState: TUserSliceSchema = {
+  isAuth: false,
+  isMounted: false,
+  role: "user",
+};
+
+export const UserSlice = createSlice({
+  name: "User",
+  initialState,
+  reducers: {
+    setAuth: (state, action: PayloadAction<boolean>) => {
+      state.isAuth = action.payload;
+      state.isMounted = true;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { actions: UserSliceActions } = UserSlice;
+export const { reducer: UserSliceReducer } = UserSlice;
