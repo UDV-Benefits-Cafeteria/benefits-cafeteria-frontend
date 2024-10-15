2e368b3d94553bae00295a940fa456e349cd6b69
profitrolka <turkin.2004@inbox.ru>
2024-10-16 01:48:11 +0500

    chore: add public key

---
+++ src/feature/RegistrationForm/index.ts
@@ -0,0 +1,3 @@
+// export {RegistrationForm} from './ui/RegistrationForm'
+export { RegistrationFormEmail } from "./ui/RegistrationFormEmail/RegistrationFormEmail.async";
+export { RegistrationFormPassword } from "./ui/RegistrationFormPassword/RegistrationFormPassword.async";
