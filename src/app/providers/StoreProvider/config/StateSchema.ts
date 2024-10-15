d5be7887c5ee96baa5a3ae24f2f131ddc4377173
profitrolka <turkin.2004@inbox.ru>
2024-10-16 01:46:19 +0500

    fix: redux

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -1,7 +1,10 @@
 import { type rtkApi } from "@shared/api/rtkApi";
 
+import type { TUserSliceSchema } from "@entity/User/model/types/User.types";
+
 export interface StateSchema {
   [rtkApi.reducerPath]: ReturnType<typeof rtkApi.reducer>;
+  user: TUserSliceSchema;
 }
 
 export type StateSchemaKeys = keyof StateSchema;
