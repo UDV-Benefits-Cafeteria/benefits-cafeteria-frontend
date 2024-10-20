4b71b6abaa753d25f507721d80dbd4658cfee1a4
profitrolka <turkin.2004@inbox.ru>
2024-10-21 03:15:14 +0500

    chore: add image

--- src/shared/Layout/UnregisteredLayout/index.tsx
+++ src/shared/Layout/UnregisteredLayout/index.tsx
@@ -1,16 +1,30 @@
 import { FC, type ReactNode } from "react";
 
+import image from "@shared/assets/images/pattern.png";
 import { classNames } from "@shared/lib/classNames/classNames";
+import { Image } from "@shared/ui/Image/Image";
 
 import styles from "./UnregisteredLayout.module.scss";
 
 type TUnregisteredLayoutProps = {
   className?: string;
+  withPattern?: boolean;
   children: ReactNode;
 };
 
 export const UnregisteredLayout: FC<TUnregisteredLayoutProps> = props => {
-  const { className, children } = props;
+  const { className, children, withPattern } = props;
 
-  return <main className={classNames(styles.page, className)}>{children}</main>;
+  return (
+    <main className={classNames(styles.page, withPattern ? styles.with_pattern : null, className)}>
+      {withPattern ? (
+        <Image
+          srs={image}
+          className={styles.pattern}
+        />
+      ) : null}
+
+      <main>{children}</main>
+    </main>
+  );
 };
