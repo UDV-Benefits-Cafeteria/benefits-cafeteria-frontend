1ddfa41912c90ff4a27b82fcc82ed2d41364f6bc
profitrolka <turkin.2004@inbox.ru>
2024-10-21 03:02:38 +0500

    fix: file name

--- src/pages/RegisterPage/RegisterPage.async.ts
+++ src/pages/RegisterPage/RegisterPage.async.ts
@@ -1,5 +1,5 @@
 import { lazy } from "react";
 
-export const RegisterPage = lazy(
-  async () => await import("./index").then(module => ({ default: module.RegisterPage }))
+export const RegisterPageAsync = lazy(
+  async () => await import("./RegisterPage").then(module => ({ default: module.RegisterPage }))
 );
